8ee36a3b730f820769eec25826ee0f0320b02da1
Tyler Reed <tylerreedytlearning@gmail.com>
2022-07-25 22:06:36 -0400

    simple changes, not using this for now because of BracketOrders

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import math
 import time
 import pandas as pd
 import asyncio
 import nest_asyncio
 import redis
+import sqlite3
+import constants
+import json
 from ib_insync import util, IB, Stock, Option
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -18,46 +22,24 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 p = r.pubsub()
 p.subscribe('tradingview')
 
+conn = sqlite3.connect('trade.db')
+cursor = conn.cursor()
+cursor.execute(constants.CREATE_TABLE)
+
+conn.commit()
+
 print("Starting up bot...")
 print("Getting initial option chains...")
+print("First Stock:", constants.AMAZON)
 
 ib = IB()
 ib.connect('127.0.0.1', 7496, clientId=1)
 ib.reqMarketDataType(1)
-contract = Stock('AAPL', 'SMART', 'USD', primaryExchange='NASDAQ')
+contract = Stock(constants.AMAZON, 'SMART', 'USD', primaryExchange='NASDAQ')
 ib.qualifyContracts(contract)
 
-[ticker] = ib.reqTickers(contract)
-print(ticker)
-
 # request a list of option chains
-chains = ib.reqSecDefOptParams(contract.symbol, '', contract.secType, contract.conId)
-# print(chains[0])
-print(util.df(chains))
-
-chain = next(c for c in chains if c.exchange == 'NASDAQBX')
-
-# print a full matrix of expirations and strikes, from this we can build all the option contracts that meet our conditions
-print(chain)
-
-# if entry price was 146.50
-entry_price = 146.50
-strikes = [strike for strike in chain.strikes
-           if strike > entry_price]
-
-print(strikes)
-
-expirations = sorted(exp for exp in chain.expirations)[:3]
-rights = ['P', 'C']
-
-print("First 3 Expirations: ", expirations)
-print("Rights: ", rights)
-
-option_contract = Option('AAPL', expirations[0], strikes[5], 'C', 'SMART', tradingClass='AAPL')
-print("Example Option Contract: ", option_contract)
-
-tickers = ib.reqTickers(option_contract)
-print("Data: ", tickers[0])
+amazon_chains = ib.reqSecDefOptParams(contract.symbol, '', contract.secType, contract.conId)
 
 current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 print("Running Live...")
@@ -77,26 +59,144 @@ async def check_messages():
     if message is not None and message['type'] == 'message':
         print("Message here on backend...")
 
+        message_data = json.loads(message['data'])
+
+        symbol = message_data['symbol']
+        price = message_data['order']['price']
+        stoploss = message_data['order']['stoploss']
+        take_profit = message_data['order']['takeProfit']
+        right = message_data['order']['right']
+        contracts = message_data['order']['contracts']
+        action = message_data['order']['action']
+        result = message_data['order']['result']
+        afterhours = message_data['order']['afterhours']
+
+        print("This is a", right, "option to", action, "for", symbol, "@", price, "and", contracts, "contracts")
+        print("Stoploss:", stoploss)
+        print("Take Profit:", take_profit)
+        print("Afterhours?:", afterhours)
+        print("Won/Loss/Pending?:", result)
+
+        # save_data(message_data, 150.0)
+
+        if action == constants.BUY:
+            if symbol == constants.AMAZON:
+                for optionschain in amazon_chains:
+                    call_strikes = [strike for strike in optionschain.strikes
+                               if strike > price]
+                    put_strikes = [strike for strike in optionschain.strikes
+                               if price - 7 < strike < price]
+                    print("All the call strikes for the current chain:", call_strikes[:7])
+                    print("All the put strikes for the current chain:", put_strikes)
+                    if right == constants.CALL:
+                        options_contract = Option(
+                            symbol,
+                            optionschain.expirations[0],
+                            call_strikes[5],
+                            right,
+                            constants.SMART,
+                            tradingClass=symbol
+                        )
+                        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+                        ticker_data = ib.reqTickers(options_contract)
+                        print(ticker_data)
+                        ask = ticker_data[0].ask
+                        delta = ticker_data[0].modelGreeks.delta
+                        gamma = ticker_data[0].modelGreeks.gamma
+                        print("All Ticker Data:", ticker_data)
+                        print("Ask price:", ask)
+                        print("Delta:", delta)
+                        print("Gamma:", gamma)
+                        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+                        # Ask + ((delta + (Gama * rounddown) * (profit - entry))
+
+                        profit = ask + ((delta + (math.floor(gamma))) * take_profit)
+                        sloss = ask - ((delta + (math.floor(gamma))) * stoploss)
+
+                        print("Options Profit:", profit)
+                        print("Options Stoploss:", sloss)
+
+                        # options_order = MarketOrder(action, contracts)
+                        # ib.placeOrder(options_contract, options_order)
+                        return
+                    else:
+                        options_contract = Option(
+                            symbol,
+                            optionschain.expirations[0],
+                            put_strikes[5],
+                            right,
+                            constants.SMART,
+                            tradingClass=symbol
+                        )
+
+                        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+                        ticker_data = ib.reqTickers(options_contract)
+                        ask = ticker_data[0].ask
+                        delta = ticker_data[0].modelGreeks.delta
+                        gamma = ticker_data[0].modelGreeks.gamma
+                        print("All Ticker Data:", ticker_data)
+                        print("Ask price:", ask)
+                        print("Delta:", delta)
+                        print("Gamma:", gamma)
+                        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+                        # Ask + ((delta + (Gama * rounddown) * (profit - entry))
+
+                        profit = ask + ((delta + (math.floor(gamma))) * take_profit)
+                        sloss = ask - ((delta + (math.floor(gamma))) * stoploss)
+
+                        print("Options Profit:", profit)
+                        print("Options Stoploss:", sloss)
+                        # options_order = MarketOrder(action, contracts)
+                        # ib.placeOrder(options_contract, options_order)
+                        return
+        else:
+            print("Only action known is BUY, we don't do anything with", action)
+
+def save_data(message_data, strike_price):
+    print("Saving to database...")
+
+    cursor = conn.cursor()
+    sqlite_insert_with_param = constants.INSERT_DATA
+    sqlite_data = (
+        message_data['symbol'],
+        message_data['order']['action'],
+        message_data['order']['right'],
+        message_data['order']['contracts'],
+        message_data['order']['price'],
+        strike_price,
+        message_data['order']['stoploss'],
+        message_data['order']['takeProfit'],
+        message_data['order']['result'],
+        message_data['order']['afterhours'],
+        time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    )
+
+    cursor.execute(sqlite_insert_with_param, sqlite_data)
+
+    conn.commit()
+
+    print("Saved to database!")
+
 # Update options chains
 async def update_options_chains():
     try:
         sched.print_jobs()
         print("Updating options chains")
-        chainz = ib.reqSecDefOptParams(contract.symbol, '', contract.secType, contract.conId)
-        print("Inside update option chains: ", chainz)
+        print(current_time)
+        amazon_chains = ib.reqSecDefOptParams(contract.symbol, '', contract.secType, contract.conId)
+        print("Updated chains: ", amazon_chains)
     except Exception as e:
         print(str(e))
 
-
-async def update_tickers():
-    try:
-        sched.print_jobs()
-        print("Updating tickers")
-        ticker_data = ib.reqTickers(option_contract)
-        print(ticker_data)
-        print("Inside ticker data")
-    except Exception as e:
-        print(str(e))
+# async def update_tickers():
+#     try:
+#         sched.print_jobs()
+#         print("Updating tickers")
+#         print(current_time)
+#         ticker_data = ib.reqTickers(option_contract)
+#         print("Current ticker data: ", ticker_data)
+#     except Exception as e:
+#         print(str(e))
 
 async def run_periodically(interval, periodic_function):
     """
@@ -107,11 +207,8 @@ async def run_periodically(interval, periodic_function):
 
 if __name__ == "__main__":
     sched = AsyncIOScheduler(daemon=True)
-    sched.add_job(update_options_chains, 'cron', minute='*')
+    sched.add_job(update_options_chains, 'cron', minute='15')
     sched.start()
-
-    # Redis connection
-    r = redis.Redis(host='localhost', port=6379, db=0)
 
     asyncio.run(run_periodically(1, check_messages))
     ib.run()
